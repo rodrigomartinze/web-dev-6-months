fcb6d60a4f0dd509fe81ee0cefa827ecc73f7eaa
martinrodrigoze <martinrodrigoze@gmail.com>
2025-06-27 23:20:30 -0600

    day-one

--- Mes-1_HTML-CSS/js/main.js
+++ Mes-1_HTML-CSS/js/main.js
@@ -1,15 +1,14 @@
 
+let textBox = document.getElementById("Data");
+
+
+function sendData() {
 
 
 
-function sendData(){
+    const age = document.getElementById("age").value;
+    const name = document.getElementById("name").value;
 
-    let textBox = document.getElementById("Data");
-
-    const age = document.getElementById("age").value ;
-    const name = document.getElementById("name").value ;
-    
     textBox.innerHTML = "";
     textBox.innerText = `Hi ${name}, I think your are ${age} years old`;
 }
-     
