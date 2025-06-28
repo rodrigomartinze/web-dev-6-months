269162a95892d9420082fd355871dfafd1515291
martinrodrigoze <martinrodrigoze@gmail.com>
2025-06-27 23:04:35 -0600

    mensaje descriptivo

---
+++ Mes-1_HTML-CSS/js/main.js
@@ -0,0 +1,11 @@
+
+
+let textBox = document.getElementById("Data")
+
+function addText(){
+
+    const age = document.getElementById("age")
+    const name = document.getElementById("name")
+    textBox.replaceWith(``)
+}
+     
