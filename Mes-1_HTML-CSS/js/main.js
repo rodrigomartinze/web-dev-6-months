b6160cbda7798de978ada68aa949326e3c742244
martinrodrigoze <martinrodrigoze@gmail.com>
2025-07-02 09:32:50 -0600

    second week

--- Mes-1_HTML-CSS/js/main.js
+++ Mes-1_HTML-CSS/js/main.js
@@ -1,30 +1,33 @@
 
-let textBox = document.getElementById("Data");
+// let textBox = document.getElementById("Data");
 
 
-function sendData() {
-
-
-
-    const age = document.getElementById("age").value;
-
-    if (age < 18) {
-        addText("and you arent big enough", age)
-    }
-    else {
-        addText("and you are enough big", age)
-    }
+// function sendData() {
 
 
 
+//     const age = document.getElementById("age").value;
+
+//     if (age < 18) {
+//         addText("and you arent big enough", age)
+//     }
+//     else {
+//         addText("and you are enough big", age)
+//     }
 
 
-    function addText(ageInfo, age) {
-        const name = document.getElementById("name").value;
-        textBox.innerHTML = "";
-        textBox.innerText = `Hi ${name}, I think your are ${age} years old ${ageInfo}`;
 
 
-    }
 
-}
+//     function addText(ageInfo, age) {
+//         const name = document.getElementById("name").value;
+//         textBox.innerHTML = "";
+//         textBox.innerText = `Hi ${name}, I think your are ${age} years old ${ageInfo}`;
+
+
+//     }
+
+// }
+
+
+
